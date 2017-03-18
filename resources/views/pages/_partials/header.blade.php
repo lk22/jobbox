5793b3352e661baf149a8a2850fa2e6cb3e68f96
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-18 14:51:03 +0100

    Updated header.blade.php.

--- resources/views/pages/_partials/header.blade.php
+++ resources/views/pages/_partials/header.blade.php
@@ -1,5 +1,5 @@
-<!-- 
-    logged out component   
+<!--
+    logged out component
  -->
 @if(Session::has('logout_success'))
     <div class="container-fluid alert alert success">
@@ -7,13 +7,13 @@
     </div>
 @endif
 
-<!-- 
+<!--
     header component
 -->
 <div class="container-fluid" id="header">
 
      <div class="col-xs-2 col-sm-2 col-md-1 col-md-offset-1 col-lg-1 col-lg-offset-1">
-         <h1 class="navbar-brand brand-name"><a href="/">{{ env('APP_NAME') }}</a></h1>
+         <h1 class="navbar-brand brand-name"><a href="{{ route('home') }}">{{ env('APP_NAME') }}</a></h1>
      </div>
 
     <div class="col-xs-10 col-sm-10 col-md-4 col-md-offset-4 col-lg-3 col-lg-offset-7">
@@ -21,7 +21,7 @@
      <ul class="menu-list">
         @if(auth()->user())
             <li class="list-item"><a href="#">{{auth()->user()->firstname}}</a></li>
-            <li class="list-item"><a href="{{ route('logout') }}">Logout</a></li>    
+            <li class="list-item"><a href="{{ route('logout') }}">Logout</a></li>
         @else
              <li class="list-item"><a href="/join">Tilmeld</a></li>
              <li class="list-item"><a href="/signin">login</a></li>
