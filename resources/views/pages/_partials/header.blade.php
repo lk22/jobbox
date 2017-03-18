4dab22dcacd5a67081b4a31e1d73a3819d019454
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-18 14:51:41 +0100

    Updated header.blade.php.

--- resources/views/pages/_partials/header.blade.php
+++ resources/views/pages/_partials/header.blade.php
@@ -13,7 +13,7 @@
 <div class="container-fluid" id="header">
 
      <div class="col-xs-2 col-sm-2 col-md-1 col-md-offset-1 col-lg-1 col-lg-offset-1">
-         <h1 class="navbar-brand brand-name"><a href="{{ route('home') }}">{{ env('APP_NAME') }}</a></h1>
+         <h1 class="navbar-brand brand-name"><a href="/"">{{ env('APP_NAME') }}</a></h1>
      </div>
 
     <div class="col-xs-10 col-sm-10 col-md-4 col-md-offset-4 col-lg-3 col-lg-offset-7">
