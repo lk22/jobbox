64716357e977009701ffab4cece37bc7673f2533
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-18 13:49:28 +0100

    Updated header.blade.php.

--- resources/views/layouts/_partials/header.blade.php
+++ resources/views/layouts/_partials/header.blade.php
@@ -1,6 +1,6 @@
   <nav id="app-header">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo">{{ config('app.name') }}</a>
+      <a href="{{ route('home') }}" class="brand-logo">{{ config('app.name') }}</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="#">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</a></li>
         <li><a href="{{ route('logout') }}">Logout</a></li>
