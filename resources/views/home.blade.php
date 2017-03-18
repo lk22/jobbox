5ac44f6b909440454a97328ff3023b8d6855d54d
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-18 19:30:44 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="container">
-        <p class="flow-text">hello {{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</p>
+        
     </div>
 
 @endsection
