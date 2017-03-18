b21461f019e8a933b40f09fde665edbb8f81bf1a
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-18 13:44:05 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,6 @@
 
 @section('content')
 
-    <nav>
-      <div class="nav-wrapper">
-        <div class="col s12">
-          <a href="#!" class="breadcrumb">First</a>
-          <a href="#!" class="breadcrumb">Second</a>
-          <a href="#!" class="breadcrumb">Third</a>
-        </div>
-      </div>
-    </nav>
+    <span class="new badge" data-badge-caption="custom caption">4</span>
 
 @endsection
