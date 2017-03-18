86835811685609168cdd04164d1b403cf2c0160d
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-18 13:40:19 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 
-<md-checkbox>content</md-checkbox>
+  <div class="fixed-action-btn horizontal" style="bottom: 45px; right: 24px;">
+      <button type="button" class="btn-floating btn-large red">
+          <i class="large mdi-editor-mode-edit"></i>
+      </button>
+      <ul>
+          <li><button type="button" class="btn-floating red"><i class="large mdi-editor-insert-chart"></i></button></li>
+          <li><button type="button" class="btn-floating red"><i class="large mdi-editor-insert-chart"></i></button></li>
+          <li><button type="button" class="btn-floating red"><i class="large mdi-editor-insert-chart"></i></button></li>
+          <li><button type="button" class="btn-floating red"><i class="large mdi-editor-insert-chart"></i></button></li>
+      </ul>
+  </div>
 
 @endsection
