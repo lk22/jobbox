55024c40ca32fd6c3691d52358f2f1f4f19aef5c
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-18 13:41:41 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,8 @@
 
 @section('content')
 
-  <div class="fixed-action-btn horizontal" style="bottom: 45px; right: 24px;">
-      <button type="button" class="btn-floating btn-large red">
-          <i class="large mdi-editor-mode-edit"></i>
-      </button>
-      <ul>
-          <li><button type="button" class="btn-floating red"><i class="large mdi-editor-insert-chart"></i></button></li>
-          <li><button type="button" class="btn-floating red"><i class="large mdi-editor-insert-chart"></i></button></li>
-          <li><button type="button" class="btn-floating red"><i class="large mdi-editor-insert-chart"></i></button></li>
-          <li><button type="button" class="btn-floating red"><i class="large mdi-editor-insert-chart"></i></button></li>
-      </ul>
-  </div>
+<button md-icon-button>
+  <md-icon class="icon"></md-icon>
+</button>
 
 @endsection
