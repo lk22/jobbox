51884c2b69829cb0d7da2f9189684c6e130ebe84
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-18 19:32:38 +0100

    Updated app.js.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,10 +14,3 @@ require('./bootstrap');
  */
 
 Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app',
-    data: {
-    	message: 'message'
-    }
-});
