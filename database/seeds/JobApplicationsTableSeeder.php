fef149f03b05b681440e2f90943118391f5cd033
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 22:18:38 +0100

    Updated JobApplicationsTableSeeder.php.

--- database/seeds/JobApplicationsTableSeeder.php
+++ database/seeds/JobApplicationsTableSeeder.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Seeder;
 
 use JobDesk\JobApplication;
 
+use Carbon\Carbon;
+
 class JobApplicationsTableSeeder extends Seeder
 {
     /**
@@ -17,6 +19,8 @@ class JobApplicationsTableSeeder extends Seeder
         JobApplication::create([
             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             'user_id' => 1
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
         ]);
 
         factory(JobApplication::class, rand(1, 10))->create(); // create job applications for the authenticated user to see
