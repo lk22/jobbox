4852cb729079b7d7e391d251575d4ede6057c8c0
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 18:24:07 +0100

    Updated webpack.config.js.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+export {
+    module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015', 'stage-0']
+				},
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/
+			}
+		]
+	},
+}
