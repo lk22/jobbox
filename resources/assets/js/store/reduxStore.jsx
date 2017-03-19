d738b703c6540eeb0e02cf5f0a4ef1b87cc0ebd1
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 07:03:10 +0100

    Updated reduxStore.jsx.

---
+++ resources/assets/js/store/reduxStore.jsx
@@ -0,0 +1,24 @@
+var redux = require('redux');
+import {applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { routerReducer } from 'react-router-redux';
+import { usersReducer, userReducer, tagsReducer, feedReducer, cookieReducer} from '../reducers/reducers.js';
+
+// config the redux store to hold state
+export var configure = (state = {}) => {
+	var reducers = redux.combineReducers({
+		routing: routerReducer, // routing reducer to keep track of the state while navigating
+		users: usersReducer,
+		user: userReducer,
+		tags: tagsReducer,
+		feed: feedReducer,
+		cookies: cookieReducer
+	});
+
+	var store = redux.createStore(reducers, state, redux.compose(
+		applyMiddleware(thunk),
+		window.devToolsExtension ? window.devToolsExtension() : f => f
+	));
+
+	return store;
+};
