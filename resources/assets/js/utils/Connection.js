767d0141d44975d3e09735d32c77ba34b888509e
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 20:23:02 +0100

    Updated Connection.js.

--- resources/assets/js/utils/Connection.js
+++ resources/assets/js/utils/Connection.js
@@ -26,11 +26,11 @@ export class Connection {
 
                 if( time < 0 ) {
 
-                    this.request.send('', window.location.url, function(response) {
+                    // this.request.send('', window.location.url, function(response) {
 
                         console.log(response);
 
-                        // this.component.html('<p>You are currently on any internet connection find a connection and try again</p>');
+                        this.component.html('<p>You are currently on any internet connection find a connection and try again</p>');
 
                     }, function(response) {
 
