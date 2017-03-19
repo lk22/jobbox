e44aca027b9e8301881403cea0b4e34b2cab7a94
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 23:33:50 +0100

    Updated JobApplicationsTableSeeder.php.

--- database/seeds/JobApplicationsTableSeeder.php
+++ database/seeds/JobApplicationsTableSeeder.php
@@ -19,6 +19,7 @@ class JobApplicationsTableSeeder extends Seeder
         JobApplication::create([
             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             'title' => 'Ansøgning til læreplads some Automekaniker',
+            'slug' => 'ansøgning-til-læreplads-som-automekaniker',
             'user_id' => 1,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
