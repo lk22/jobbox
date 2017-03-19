aa5a0193a878c69d268133e1c2ecbe6fbeb64012
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 22:18:03 +0100

    Updated JobApplicationsTableSeeder.php.

--- database/seeds/JobApplicationsTableSeeder.php
+++ database/seeds/JobApplicationsTableSeeder.php
@@ -13,6 +13,12 @@ class JobApplicationsTableSeeder extends Seeder
      */
     public function run()
     {
+
+        JobApplication::create([
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'user_id' => 1
+        ]);
+
         factory(JobApplication::class, rand(1, 10))->create(); // create job applications for the authenticated user to see
     }
 }
