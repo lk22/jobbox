020bf266e6779846177a4df34c873219f8417e82
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 22:18:50 +0100

    Updated JobApplicationsTableSeeder.php.

--- database/seeds/JobApplicationsTableSeeder.php
+++ database/seeds/JobApplicationsTableSeeder.php
@@ -18,7 +18,7 @@ class JobApplicationsTableSeeder extends Seeder
 
         JobApplication::create([
             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'user_id' => 1
+            'user_id' => 1,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
         ]);
