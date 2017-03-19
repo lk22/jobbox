65372053ee32261fcb856ed041e8cc5fc55aa3be
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 19:03:39 +0100

    Updated Helper.js.

--- resources/assets/js/Helper.js
+++ resources/assets/js/Helper.js
@@ -1,63 +1,10 @@
-import Axios from 'axios';
+
 import Request from './helpers/Request.js';
 
-class Helper {
+export class Helper {
     constructor(){
         this.request = new Request;
     }
 
-    sendAxiosRequest(method, url, data = null, response) {
-        switch (method) {
-            case 'POST':
-                this.sendPostRequest(url, data, response)
-                break;
 
-            case 'PUT':
-                this.sendUpdateRequest(url, data, response);
-                break;
-
-            case 'DELETE':
-                this.sendDeleteRequest(url, response);
-            default:
-                this.sendGetRequestTo(url, response);
-            break;
-        }
-    }
-
-    getResponse(response) {
-        return response;
-        console.log(response);
-    }
-
-    sendGetRequestTo(url, response) {
-        Axios.get(url).then((response) => {
-            this.getResponse(response);
-        }).catch((response) => {
-            this.getResponse();
-        });
-    }
-
-    sendPostRequest(url, data = {}, response) {
-        Axios.post(url, data).then(( response ) => {
-            this.getResponse(response);
-        }).catch(( response ) => {
-            this.getResponse(response);
-        });
-    }
-
-    sendUpdateRequest(url, data = {}, response) {
-        Axios.put(url, data).then(( response ) => {
-            this.getResponse(response);
-        }).catch(( response ) => {
-            this.getResponse(response);
-        });
-    }
-
-    sendDeleteRequest(url, response) {
-        Axios.delete(url).then(( response ) => {
-            this.getResponse(response);
-        }).catch(( response ) => {
-            this.getResponse(response);
-        });
-    }
 }
