f08b6b405f65571694c39f8206c209a184f31c0a
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 00:02:17 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,7 +29,7 @@
                 </div>
             </div>
 
-            <div class="col l4 push-l3">
+            <div class="col l3 push-l3">
                 <div class="row">
                     <div class="job-application-list">
                         @if (count($jobs))
