cd6eb2dcf863b94262cde72b26becc2428142ba1
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 21:55:43 +0100

    Updated 2017_03_19_204817_create_job_applications_table.php.

--- database/migrations/2017_03_19_204817_create_job_applications_table.php
+++ database/migrations/2017_03_19_204817_create_job_applications_table.php
@@ -20,7 +20,6 @@ class CreateJobApplicationsTable extends Migration
             $table->timestamps();
             $table->softDeletes();
 
-
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
