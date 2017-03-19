0492c0f6fcfed78f2b6f35535908e59acd3f4b0c
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 21:28:15 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,15 @@
 
     <div class="container">
         <div class="row">
-            <div class="col l3">
+            <div class="col l3 push-l1">
                 hello
             </div>
 
-            <div class="col l3">
+            <div class="col l3 push-l1">
                 world
             </div>
 
-            <div class="col l3">
+            <div class="col l3 push-l1">
                 {{ auth()->user()->firstname }}
             </div>
         </div>
