6d0fc10b6ef5c080173247003acb5d3132452669
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 22:40:11 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-    <div class="container-fluid" id="home">
+    <div class="container" id="home">
         <div class="row">
 
 
-            <div class="col l6 push-l2">
+            <div class="col l6">
                 <div class="row">
                     <div class="job-application-list">
                         <!-- applications list goes here -->
@@ -34,7 +34,7 @@
                 </div>
             </div>
 
-            <div class="col l6 push-l3">
+            <div class="col l6">
                 <p class="text-center">World</p>
             </div>
         </div>
