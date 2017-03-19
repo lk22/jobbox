798c33ff170c25650db422384c6b4851133bacda
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 23:35:06 +0100

    Updated JobApplicationController.php.

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -4,7 +4,12 @@ namespace JobDesk\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Jobdesk\JobApplication;
+
 class JobApplicationController extends Controller
 {
-    //
+    public function __construct(JobApplication $job)
+    {
+        $this->job = $job;
+    }
 }
