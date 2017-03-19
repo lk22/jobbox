1f3ca5572ae25f00aa34d05558598354b5fbd081
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 21:27:29 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,19 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col l4">
-            hello
-        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col l4">
+                hello
+            </div>
 
-        <div class="col l4">
-            world
-        </div>
+            <div class="col l4">
+                world
+            </div>
 
-        <div class="col l4">
-            {{ auth()->user()->firstname }}
+            <div class="col l4">
+                {{ auth()->user()->firstname }}
+            </div>
         </div>
     </div>
 
