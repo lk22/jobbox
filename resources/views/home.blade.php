380c5de34fdf78ed392b8a17015a82224513abc7
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 23:01:37 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-    <div class="container-flud" id="home">
+    <div class="container-fluid" id="home">
         <div class="row">
 
             <div class="col s12 m12 l12 profile header">
 
             </div>
 
-            <div class="col l6">
+            <div class="col l4 push-l2">
                 <div class="row">
                     <div class="job-application-list">
                         @if (count($jobs))
@@ -31,7 +31,7 @@
                 </div>
             </div>
 
-            <div class="col l6">
+            <div class="col l4 push-l2">
                 <p class="text-center">World</p>
             </div>
         </div>
