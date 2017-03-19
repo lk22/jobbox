ce8fb5f8cab8fc394d42c6440f11f5c90ea5c560
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 21:56:55 +0100

    Updated JobApplicationsTableSeeder.php.

---
+++ database/seeds/JobApplicationsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use JobDesk\JobApplication;
+
+class JobApplicationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
