d741fa12b2356caf898ffcfaf59c96039e138898
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 00:19:37 +0100

    Updated app.blade.php.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,15 +53,15 @@
         @include('layouts._partials.header')
         @include('layouts._partials.ethernetComponent')
         <div class="preloader-wrapper big active">
-      <div class="spinner-layer spinner-blue">
-        <div class="circle-clipper left">
-          <div class="circle"></div>
-        </div><div class="gap-patch">
-          <div class="circle"></div>
-        </div><div class="circle-clipper right">
-          <div class="circle"></div>
+          <div class="spinner-layer spinner-blue">
+            <div class="circle-clipper left">
+              <div class="circle"></div>
+            </div><div class="gap-patch">
+              <div class="circle"></div>
+            </div><div class="circle-clipper right">
+              <div class="circle"></div>
+            </div>
         </div>
-      </div>
 
         <div id="app" style="padding-top:2rem; background: #DFEFF9;">
             @yield('content')
