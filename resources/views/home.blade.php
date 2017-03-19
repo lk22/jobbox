c32547d6bab3a4748c0c865f7ba98b4a6c84b0e0
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 21:19:38 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,13 @@
 
     <div class="container">
         <div class="row">
-            
+            <div class="col s3">
+
+            </div>
+
+            <div class="col s9">
+
+            </div>
         </div>
     </div>
 
