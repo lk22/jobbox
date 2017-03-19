2b80832b057d318f24011d8aa34f7ffc350ee82e
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 21:13:03 +0100

    Updated app.blade.php.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,7 +59,7 @@
 
     <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="{{ asset('js/jobdesk.js') }}"></script>
+    <!-- <script src="{{ asset('js/jobdesk.js') }}"></script> -->
 
     <script type="text/javascript">
         if(navigator.online === false) {
