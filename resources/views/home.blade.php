b2a7395b2878094adbced230c1d9ae623ba18358
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 00:02:06 +0100

    Updated home.blade.php.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid" id="home">
         <div class="row">
 
-            <div class="col s12 m10 push-m3 l10 push-l2 profile header">
+            <div class="col s12 m10 push-m3 l10 push-l3 profile header">
                 <div class="row">
                     <div class="col s2 m2 l2 avatar">
                         <div class="row">
@@ -29,7 +29,7 @@
                 </div>
             </div>
 
-            <div class="col l4 push-l2">
+            <div class="col l4 push-l3">
                 <div class="row">
                     <div class="job-application-list">
                         @if (count($jobs))
