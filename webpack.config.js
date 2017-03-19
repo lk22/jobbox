5efc5ee8c30896e05328cdb734524173721f3ffb
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 18:26:10 +0100

    Updated webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,6 @@
 module.export {
     resolve: {
 		root: __dirname,
-		alias: {
-			// components
-			Main: 'app/resources/assets/js/components/Main.jsx',
-			Header: 'app/resources/assets/js/components/Header.jsx',
-
-			actions: 'app/resources/assets/js/actions/actions.jsx',
-			reducers: 'app/resources/assets/js/reducers/reducers.jsx',
-			store: 	  'app/resources/assets/js/store/storeConfig.jsx',
-		},
 		extensions: ['', '.js', '.jsx']
 	},
     module: {
