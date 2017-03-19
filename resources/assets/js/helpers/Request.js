1a5881b27ead7695fde65b019caf7233030e0337
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-19 18:49:06 +0100

    Updated Request.js.

--- resources/assets/js/helpers/Request.js
+++ resources/assets/js/helpers/Request.js
@@ -1,1 +1,58 @@
 import Axios from 'axios';
+
+class Request {
+    sendAxiosRequest(method, url, data = null, response) {
+        switch (method) {
+            case 'POST':
+                this.sendPostRequest(url, data, response)
+                break;
+
+            case 'PUT':
+                this.sendUpdateRequest(url, data, response);
+                break;
+
+            case 'DELETE':
+                this.sendDeleteRequest(url, response);
+            default:
+                this.sendGetRequestTo(url, response);
+            break;
+        }
+    }
+
+    getResponse(response) {
+        return response;
+        console.log(response);
+    }
+
+    sendGetRequestTo(url, response) {
+        Axios.get(url).then((response) => {
+            this.getResponse(response);
+        }).catch((response) => {
+            this.getResponse();
+        });
+    }
+
+    sendPostRequest(url, data = {}, response) {
+        Axios.post(url, data).then(( response ) => {
+            this.getResponse(response);
+        }).catch(( response ) => {
+            this.getResponse(response);
+        });
+    }
+
+    sendUpdateRequest(url, data = {}, response) {
+        Axios.put(url, data).then(( response ) => {
+            this.getResponse(response);
+        }).catch(( response ) => {
+            this.getResponse(response);
+        });
+    }
+
+    sendDeleteRequest(url, response) {
+        Axios.delete(url).then(( response ) => {
+            this.getResponse(response);
+        }).catch(( response ) => {
+            this.getResponse(response);
+        });
+    }
+}
