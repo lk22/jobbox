252974988c3738db907b792ac95ad24d6a457caf
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 00:04:18 +0100

    Updated app.blade.php.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
         @include('layouts._partials.header')
         @include('layouts._partials.ethernetComponent')
 
-        <div id="app" style="margin-top:2rem; background: DFEFF9;">
+        <div id="app" style="padding-top:2rem; background: #DFEFF9;">
             @yield('content')
         </div>
     </div>
