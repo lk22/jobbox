4e0b9bf96a02bf55c162607fdec94292c19e6e55
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 01:09:53 +0100

    Updated JobApplicationController.php.

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -19,7 +19,7 @@ class JobApplicationController extends Controller
     /**
     * finding the job application by slug property
     */
-    public function job($user_slug, $slug)
+    public function job($slug)
     {
         $job = $this->job->whereSlug($slug)->firstOrFail();
 
