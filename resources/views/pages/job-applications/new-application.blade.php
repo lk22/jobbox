acf2bc61812f54533c4cd33b517b5ce0386636f6
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 13:45:48 +0100

    Updated new-application.blade.php.

--- resources/views/pages/job-applications/new-application.blade.php
+++ resources/views/pages/job-applications/new-application.blade.php
@@ -9,7 +9,7 @@
             </a>
             <ul>
               <li><a class="btn-floating red"><i class="material-icons">insert_comment</i></a></li>
-              <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+              <li><a class="btn-floating yellow darken-1"><i class="material-icons">save</i></a></li>
               <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
               <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
             </ul>
