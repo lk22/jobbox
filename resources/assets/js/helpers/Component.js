704f8b830d7c7f76092ed92a5353f3d1fbb37d51
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 14:51:31 +0100

    Updated Component.js.

--- resources/assets/js/helpers/Component.js
+++ resources/assets/js/helpers/Component.js
@@ -1,16 +1,14 @@
+import Editor from './../components/Editor.js';
+
 export class Component {
 
     constructor(element){
-        this.get(element);
-    }
-
-    /**
-     * get element
-     * @param  {[type]} element [description]
-     * @return [type]           [description]
-     */
-    get(element) {
         return $(element);
     }
+
+    on(element) {
+        return $(element);
+    }
+
 }
 export default Component;
