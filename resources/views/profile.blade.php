a50344538d5b9e812f4e97dca67d8a53e3e5c638
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 12:42:51 +0100

    Updated profile.blade.php.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -30,29 +30,6 @@
             </div>
 
             <div class="col l3 push-l2">
-                <div class="job-application-list">
-                    @if (count($jobs))
-                        <table class="responsive-table">
-                            <thead>
-                                <td>Job Ansøgninger</td>
-                            </thead>
-                            <tbody>
-                                @foreach ($jobs as $job)
-                                    <tr>
-                                        <td>
-                                            <a href="{{ route('job', [Auth::user()->slug, $job->slug]) }}">
-                                                {{ substr($job->title, 0, 50) }}
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    @endif
-                </div>
-            </div>
-
-            <div class="col l3 push-l2">
                 <p class="left-align">World</p>
             </div>
         </div>
@@ -90,7 +67,7 @@
       </div>
 
       <div class="modal-footer">
-          <a href="{{ route('new.application', Auth::user()->slug) }}" class="modal-action modal-close waves-effect waves-green btn-green"><i class="material-icons">open_in_new CREATE</i></a>
+          <a href="{{ route('new.application', Auth::user()->slug) }}" class="modal-action modal-close waves-effect waves-green btn-green"><i class="material-icons">open_in_new</i></a>
           <a href="#" class="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
       </div>
   </div>
