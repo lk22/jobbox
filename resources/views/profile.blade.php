173f6ce8cdaff9873fe139fed35479970c734cb6
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 12:05:52 +0100

    Updated profile.blade.php.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -43,6 +43,34 @@
 
   <a class="waves-effect waves-light btn" href="#jobsModal">Modal</a>
 
-  @include('layouts._partials.modals.jobsmodal')
+  <!-- jobs list modal  -->
+  <div id="jobsModal" class="modal bottom-sheet">
+      <div class="modal-content">
+          <div class="job-application-list">
+              @if (count($jobs))
+                  <table class="responsive-table">
+                      <thead>
+                          <td>Job Ansøgninger</td>
+                      </thead>
+                      <tbody>
+                          @foreach ($jobs as $job)
+                              <tr>
+                                  <td>
+                                      <a href="{{ route('job', [Auth::user()->slug, $job->slug]) }}">
+                                          {{ substr($job->title, 0, 50) }}
+                                      </a>
+                                  </td>
+                              </tr>
+                          @endforeach
+                      </tbody>
+                  </table>
+              @endif
+          </div>
+      </div>
+      <div class="modal-footer">
+          <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+      </div>
+  </div>
+
 
 @endsection
