397d9dc69b48b5efcfb435d444cd9d005816a22c
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 01:14:03 +0100

    Updated header.blade.php.

--- resources/views/layouts/_partials/header.blade.php
+++ resources/views/layouts/_partials/header.blade.php
@@ -2,13 +2,13 @@
     <div class="nav-wrapper">
       <a href="{{ route('home') }}" class="brand-logo">{{ config('app.name') }}</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="#">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</a></li>
+        <li>
+            <div class="chip">
+              <img src="{{ Auth::user()->avatar }}" alt="Contact Person">
+              <li><a href="#">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</a></li>
+            </div>
+        </li>
         <li><a href="{{ route('logout') }}">Logout</a></li>
       </ul>
     </div>
   </nav>
-
-  <div class="chip">
-    <img src="{{ Auth::user()->avatar }}" alt="Contact Person">
-    <li><a href="#">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</a></li>
-  </div>
