90f79d6e00c6599ca0da0be9f31781446ad24182
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 12:31:49 +0100

    Updated profile.blade.php.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -88,9 +88,9 @@
               @endif
           </div>
       </div>
-      
+
       <div class="modal-footer">
-          <a href="{{ route('new.application', Auth::user()->slug }}" class="modal-action modal-close waves-effect waves-green btn-green">Close</a>
+          <a href="{{ route('new.application', ) }}" class="modal-action modal-close waves-effect waves-green btn-green">Close</a>
           <a href="#" class="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
       </div>
   </div>
