d3b20eb619a72b993324abf1fe04ddb5c151253f
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 12:17:35 +0100

    Updated profile.blade.php.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -96,7 +96,7 @@
   </div>
 
   <!-- Modal Trigger -->
-  <a class="modal-trigger waves-effect waves-light btn" href="#modal1">Modal</a>
+  <a data-target="modal" class="modal-trigger waves-effect waves-light btn" href="#modal1">Modal</a>
 
   <!-- Modal Structure -->
   <div id="modal1" class="modal">
