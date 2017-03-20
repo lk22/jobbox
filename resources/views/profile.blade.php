686083126f59756172cb354b31e6a98d26cd5b2c
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 12:10:48 +0100

    Updated profile.blade.php.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -30,6 +30,29 @@
             </div>
 
             <div class="col l3 push-l2">
+                <div class="job-application-list">
+                    @if (count($jobs))
+                        <table class="responsive-table">
+                            <thead>
+                                <td>Job Ansøgninger</td>
+                            </thead>
+                            <tbody>
+                                @foreach ($jobs as $job)
+                                    <tr>
+                                        <td>
+                                            <a href="{{ route('job', [Auth::user()->slug, $job->slug]) }}">
+                                                {{ substr($job->title, 0, 50) }}
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                </div>
+            </div>
+
+            <div class="col l3 push-l2">
                 <p class="left-align">World</p>
             </div>
         </div>
