4cd08a8942d419bb207e677b210c90113e83f7c3
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 11:52:35 +0100

    Updated profile.blade.php.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,6 @@
     <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-          
     <script src="{{ asset('js/jobdesk.js') }}"></script>
 </body>
 </html>
