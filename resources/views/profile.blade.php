3a0a53ecb9d37d6924295d8340d006c2cf6a21c9
Leo Knudsen <nightskater-220@hotmail.com>
2017-03-20 12:41:39 +0100

    Updated profile.blade.php.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -90,7 +90,7 @@
       </div>
 
       <div class="modal-footer">
-          <a href="{{ route('new.application', Auth::user()->slug) }}" class="modal-action modal-close waves-effect waves-green btn-green">Create<i class="material-icons">open_in_new</i></a>
+          <a href="{{ route('new.application', Auth::user()->slug) }}" class="modal-action modal-close waves-effect waves-green btn-green">CREATE<i class="material-icons">open_in_new</i></a>
           <a href="#" class="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
       </div>
   </div>
