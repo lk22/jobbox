a2e089a1f26e9bdea463d2c169108cefe398103a
Leo Knudsen <nightskater-220@hotmail.com>
2017-04-05 20:36:16 +0200

    Updated logoutModal.blade.php.

--- resources/views/pages/_partials/modals/logoutModal.blade.php
+++ resources/views/pages/_partials/modals/logoutModal.blade.php
@@ -5,8 +5,7 @@
               <h1>Are you sure?</h1>
           </div>
       <div class="modal-footer">
-        <a target="_blank" href="{{ route('new.application', Auth::user()->slug) }}" class="modal-action modal-close waves-effect waves-green btn-green"><i class="material-icons">open_in_new</i></a>
-        <a href="#" class="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+        <a href="/" class="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
      </div>
   </div>
 </div>
