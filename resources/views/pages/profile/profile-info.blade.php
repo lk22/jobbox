6d2d930d0c77be8f93bf066fa6439f0bbb47c18f
Leo Knudsen <nightskater-220@hotmail.com>
2017-04-06 21:27:31 +0200

    Updated profile-info.blade.php.

--- resources/views/pages/profile/profile-info.blade.php
+++ resources/views/pages/profile/profile-info.blade.php
@@ -13,35 +13,37 @@
             <p class="center-align">{{ $auth->description }}</p>
           </div>
 
+          <div class="email center-align">
+            <p> Email: <a href="mailto:{{ $auth->email }}"> {{ $auth->email }}</a></p>
+          </div>
+
+          <div class="dream-job center-align">
+
+            @if($auth->dream_job_title) <p>Dream job: {{ $auth->dream_job_title }}</p> @endif
+            @if(!$auth->dream_job_title) <p></p> @endif
+
+          </div>
+
+          <div class="current-job center-align">
+            @if($auth->job_position) <p>Working as: {{ $auth->job_position }} <br> at {{ $auth->company }}</p> @endif
+            @if(!$auth->job_position) <p>You are unemployed</p> @endif
+          </div>
+
+          <div class="job-applications center-align">
+            @if(count($jobs) > 1)
+              <p class="job-count">You have currently {{ count($jobs) }} job applications - <a data-target="jobsModal" href="#jobsModal" class="modal-trigger jobs-modal-btn">see here</a></p>
+            @else
+              <p class="job-count">You have currently {{ count($jobs) }} job application - <a data-target="jobsModal" href="#jobsModal" class="modal-trigger jobs-modal-btn">see here</a></p>
+            @endif
+          </div>
+
+          <div class="update-user">
+              <a data-target="updateUserModal" href="#updateUserModal" class="update-user-btn">Edit <i class="material-icons">setting</i></a>
+          </div>
+
       </div>
   </div>
   <div class="col s12 m12 l5 pull-l-2profile-information-header z-depth-3">
-      <div class="email center-align">
-        <p> Email: <a href="mailto:{{ $auth->email }}"> {{ $auth->email }}</a></p>
-      </div>
 
-      <div class="dream-job center-align">
-
-        @if($auth->dream_job_title) <p>Dream job: {{ $auth->dream_job_title }}</p> @endif
-        @if(!$auth->dream_job_title) <p></p> @endif
-
-      </div>
-
-      <div class="current-job center-align">
-        @if($auth->job_position) <p>Working as: {{ $auth->job_position }} <br> at {{ $auth->company }}</p> @endif
-        @if(!$auth->job_position) <p>You are unemployed</p> @endif
-      </div>
-
-      <div class="job-applications center-align">
-        @if(count($jobs) > 1)
-          <p class="job-count">You have currently {{ count($jobs) }} job applications - <a data-target="jobsModal" href="#jobsModal" class="modal-trigger jobs-modal-btn">see here</a></p>
-        @else
-          <p class="job-count">You have currently {{ count($jobs) }} job application - <a data-target="jobsModal" href="#jobsModal" class="modal-trigger jobs-modal-btn">see here</a></p>
-        @endif
-      </div>
-
-      <div class="update-user">
-          <a data-target="updateUserModal" href="#updateUserModal" class="update-user-btn">Edit <i class="material-icons">setting</i></a>
-      </div>
   </div>
 </div>
