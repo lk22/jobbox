34ad572c9a5815e94a1f565ba47c7b771eaa90d6
Leo Knudsen <nightskater-220@hotmail.com>
2017-04-06 21:32:03 +0200

    Updated profile-job-applications.blade.php.

--- resources/views/pages/profile/profile-job-applications.blade.php
+++ resources/views/pages/profile/profile-job-applications.blade.php
@@ -2,7 +2,7 @@
   <div class="col s12 m12 l5 profile-job-applications__header z-depth-3">
       <div class="heading">
           <h5>Job ansøgniner</h5>
-          @if(count($jobs))<h6>du har {{}}</h6>
+          @if(count($jobs))<h6>du har {{ count($jobs) }} job ansøgning(er)</h6>@endif
       </div>
   </div>
 </div>
