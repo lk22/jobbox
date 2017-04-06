5cef45ec2dc840a1b692a91ed10aad4947ced1a5
Leo Knudsen <nightskater-220@hotmail.com>
2017-04-06 21:10:24 +0200

    Updated logoutModal.blade.php.

--- resources/views/pages/_partials/modals/logoutModal.blade.php
+++ resources/views/pages/_partials/modals/logoutModal.blade.php
@@ -2,10 +2,11 @@
   <div id="logoutModal" class="modal">
       <div class="modal-content">
           <div class="job-application-list">
-              <h1>Are you sure?</h1>
+              <h3>Are you sure?</h3>
+              <p>Are your sure you want to logout if u have anything in proccess it will be lost</p>
           </div>
       <div class="modal-footer">
-        <a href="/logout" class="modal-action modal-close waves-effect waves-green btn-flat">yes</a>
+        <a href="/logout" class="modal-action modal-close waves-effect waves-green btn-flat">Logout</a>
      </div>
   </div>
 </div>
