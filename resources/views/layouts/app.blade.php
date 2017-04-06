cb5d78f55511742ba1c09c057628b54bb3aa9144
Leo Knudsen <nightskater-220@hotmail.com>
2017-04-06 21:19:56 +0200

    Updated app.blade.php.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
         @include('layouts._partials.header')
         @include('layouts._partials.ethernetComponent')
 
-        <div id="app" class="cyan white" style="min-height:650px; padding-bottom:2rem; background: #bdc3c7; overflow-x:hidden;">
+        <div id="app" class="cyan white" style="min-height:650px; padding-bottom:2rem; background: #95a5a6; overflow-x:hidden;">
             @yield('content')
         </div>
     </div>
